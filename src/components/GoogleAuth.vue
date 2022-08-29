6d01f5de2780e7bd63d9d22b261eb1627c23ab86
nirajraba212 <nirajraba212@gmail.com>
2022-08-29 20:02:00 +0530

    main changes

---
+++ src/components/GoogleAuth.vue
@@ -0,0 +1,86 @@
+<template>
+  <span>
+    <!-- Left empty intentionally -->
+    <div class="g-signin2" data-onsuccess="onSignIn"></div>
+  </span>
+</template>
+
+<script>
+//var apiUrl = "https://apis.google.com/js/platform.js";
+
+export default {
+  name: "GoogleAuth",
+  props: {
+    config: { type: Object }
+    // apiKey: {type: String},
+    // discoveryDocs: {type: [String]},
+    // clientId: {type: String},
+    // scopes: {type: String},
+  },
+  created: function() {
+    
+    //this.installClient();
+  },
+  data: function() {
+    return {
+      GoogleAuth: null,
+      GoogleUser: null
+    };
+  },
+
+  methods: {
+    async login() {
+      // if (this.GoogleAuth.isSignedIn.get()) {
+      //   this.GoogleUser = this.GoogleAuth.currentUser.get();
+      // } else {
+      //   this.GoogleUser = await this.GoogleAuth.signIn();
+      // }
+      // return this.GoogleUser.getAuthResponse().access_token;
+    },
+    async logout() {
+      //await this.GoogleAuth.signOut();
+    },
+    installClient() {
+      // var script = document.createElement("script");
+      // script.src = apiUrl;
+      // var self = this;
+      // script.onreadystatechange = script.onload = function() {
+      //   console.log("load",window.gapi);
+        
+      //   window.gapi.load('auth2', function() {
+      //     self.initClient();
+      //   });
+        
+      // };
+      // document.getElementsByTagName("head")[0].appendChild(script);
+    },
+    initClient() {
+      //var self = this;
+      //if(window.gapi && window.gapi.auth2) {
+        //  window.gapi.auth2.init({ client_id: "705476648316-fba0luj0192jaombs8p9a54nf20tvod5.apps.googleusercontent.com" })
+        //   .then(function(GoogleAuth) {
+        //     console.log("GoogleAuth",GoogleAuth);
+        //     GoogleAuth.then(function(onInit,onError){
+        //       console.log("onInit",onInit);
+        //       console.log("onError",onError);
+        //       //self.GoogleAuth = window.gapi.auth2.GoogleAuth;
+        //       //self.$emit("authInitialized");
+        //     });
+            
+        //   })
+        //   .catch(error => {
+        //     console.log(error);
+        //   });
+      //}
+      
+    },
+    onSignIn(googleUser) {
+      var profile = googleUser.getBasicProfile();
+      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+      console.log('Name: ' + profile.getName());
+      console.log('Image URL: ' + profile.getImageUrl());
+      console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+    }
+  }
+};
+</script>
---
+++ src/components/GoogleAuthDemo.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="googleAuthDemo">
+    <div>
+      <h1>Google Auth</h1>
+      <GoogleAuth
+        :config="authConfig"
+        @authInitialized="authInitialized"
+        ref="authRef"
+      />
+      
+    </div>
+  </div>
+</template>
+
+<script>
+import GoogleAuth from "./GoogleAuth.vue";
+export default {
+  name: "GoogleAuthDemo",
+  components: { GoogleAuth },
+  data: function() {
+    return {
+      accessToken: null,
+      initialized: false,
+      authConfig: {
+        apiKey: "AIzaSyBSI6J62iY7QMSErhw_QumFprwWzQFv8XQ",
+        discoveryDocs: [],
+        clientId:
+          "705476648316-fba0luj0192jaombs8p9a54nf20tvod5.apps.googleusercontent.com",
+        scopes: "https://www.googleapis.com/auth/userinfo.email",
+        plugin_name: "Google Login"
+      }
+    };
+  },
+  methods: {
+    authInitialized() {
+      this.initialized = true;
+    },
+    // async login() {
+    //   this.accessToken = await this.$refs.authRef.login();
+    //   console.log(this.accessToken);
+    // },
+    // async logout() {
+    //   await this.$refs.authRef.logout();
+    //   this.accessToken = null;
+    // }
+  }
+};
+</script>
